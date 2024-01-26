d114821c8e2a819e2e220d686cc77a41dc26d32f
eddy8mame <132296192+eddy8mame@users.noreply.github.com>
2024-01-25 19:20:49 -0500

    Preparing to merge

--- client/components/CollapsiblePanel/CollapsiblePanel.tsx
+++ client/components/CollapsiblePanel/CollapsiblePanel.tsx
@@ -19,31 +19,18 @@ export const CollapsiblePanel = ({ title, children }: Props) => {
           aria-controls={title.replace(/\s+/g, '') + '-content'}
                   id={title.replace(/\s+/g, '') + '-header'}
                   sx={{
-                      fontSize: "1.25rem",
-                      margin: "20px 20px 20px 0px",
-                      '& .MuiAccordionSummary-content': {
-                          margin: "20px 0 0 20px"
-                      },
-                      '& .css-o4b71y-MuiAccordionSummary-content.Mui-expanded': {
-                        margin: "20px 0 0px 20px"
-                      },
-                      '& .MuiAccordionSummary.Mui-expanded': {
-                        margin: "20px 0 0px 20px"
-                      }
-                  }}
-                //   sx={{
-                //     width: '100%',
-                //     height: '100%',
-                //     '& .MuiChartsAxis-root': {
-                //       stroke: 'black',
+                    fontSize: "1.25rem",
+                    '& .MuiAccordionSummary.Mui-expanded': {
+                      margin: "20px 0 0px 20px"
+                    }
+                }}
         >
           {title}
         </AccordionSummary>
         <AccordionDetails
           sx={{
             display: 'grid',
-                      gridTemplateColumns: '3fr 1fr',
-            gridTemplateRows: 'auto 1fr',
+            gridTemplateColumns: '1fr 1fr',
             gap: '1rem',
           }} // TODO: Verify this is correct
         >
